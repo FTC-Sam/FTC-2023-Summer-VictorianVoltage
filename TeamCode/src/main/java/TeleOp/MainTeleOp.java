c73cc822a6280bebfd4e7ca9f134a97a4cb2bd61
samftc <chenxiang.xu56@k12.leanderisd.org>
2023-02-11 14:31:13 -0600

    Reforged gamepad is basically implicit listener. A listener runs in the middle of Scheduler, scheduler runs beforeach first, then listeners, lastly aftereach which is just the block after Scheduler.launch(this, ()-> {//here});

--- TeamCode/src/main/java/TeleOp/MainTeleOp.java
+++ TeamCode/src/main/java/TeleOp/MainTeleOp.java
@@ -1,14 +1,41 @@
 package TeleOp;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-public class MainTeleOp extends LinearOpMode {
+import ftc.rogue.blacksmith.BlackOp;
+import ftc.rogue.blacksmith.Scheduler;
+import ftc.rogue.blacksmith.annotations.CreateOnGo;
+import ftc.rogue.blacksmith.listeners.Listener;
+import ftc.rogue.blacksmith.listeners.ReforgedGamepad;
+import mechanisms.DriveTrain;
+
+@TeleOp (name = "MainTeleOp")
+public class MainTeleOp extends BlackOp {
+
+    ReforgedGamepad driver = new ReforgedGamepad(gamepad1);
+    ReforgedGamepad codriver = new ReforgedGamepad(gamepad2);
+
+    @CreateOnGo
+    DriveTrain dt;
+
 
 
 
 
     @Override
-    public void runOpMode() {
+    public void go() {
+        Scheduler.beforeEach(() -> {
+            dt.drive(driver);
+        });
+
+        //implicit listener run
+
+        Scheduler.launchOnStart(this, () -> {
+            //aftereach, runs after every listener, normally pid
+        });
+
 
     }
+
+
 }
---
+++ TeamCode/src/main/java/mechanisms/DriveTrain.java
@@ -0,0 +1,103 @@
+package mechanisms;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import ftc.rogue.blacksmith.listeners.ReforgedGamepad;
+
+public class DriveTrain {
+
+    private DcMotorEx motorFrontLeft, motorBackLeft, motorFrontRight, motorBackRight;
+
+
+
+    public DriveTrain() {
+
+        motorFrontLeft = (DcMotorEx) hardwareMap.dcMotor.get("FL");
+        motorBackLeft = (DcMotorEx) hardwareMap.dcMotor.get("BL");
+        motorFrontRight = (DcMotorEx) hardwareMap.dcMotor.get("FR");
+        motorBackRight = (DcMotorEx) hardwareMap.dcMotor.get("BR");
+
+        motorFrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorBackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorFrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorBackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        motorFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        motorFrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorBackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorFrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorBackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+    }
+
+
+
+    public void drive(ReforgedGamepad gamepad) {
+
+        double y = -gamepad.left_stick_y.get(); // Remember, this is reversed!
+        double x = gamepad.left_stick_x.get();
+        double rx = gamepad.right_stick_x.get();
+        double frontLeftPower;
+        double frontRightPower;
+        double backLeftPower;
+        double backRightPower;
+
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio, but only when
+        // at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+
+        // Calculate the mecanum motor powers
+        frontLeftPower = (y + x + 2 * rx) / denominator;
+        backLeftPower = (y - x + 2 * rx) / denominator;
+        frontRightPower = (y - x - 2 * rx) / denominator;
+        backRightPower = (y + x - 2 * rx) / denominator;
+
+        // Cube the motor powers
+        frontLeftPower = Math.pow(frontLeftPower, 3);
+        frontRightPower = Math.pow(frontRightPower, 3);
+        backLeftPower = Math.pow(backLeftPower, 3);
+        backRightPower = Math.pow(backRightPower, 3);
+
+        // Calculate the maximum value of all the motor powers
+        // The argument here is just an array separated into different lines
+        double maxValue = getMax(new double[]{
+                frontLeftPower,
+                frontRightPower,
+                backLeftPower,
+                backRightPower
+        });
+
+        // Resize the motor power values
+        if (maxValue > 1) {
+            frontLeftPower /= maxValue;
+            frontRightPower /= maxValue;
+            backLeftPower /= maxValue;
+            backRightPower /= maxValue;
+        }
+
+        motorFrontLeft.setPower(frontLeftPower);
+        motorBackLeft.setPower(backLeftPower);
+        motorFrontRight.setPower(frontRightPower);
+        motorBackRight.setPower(backRightPower);
+
+    }
+
+
+
+    private double getMax(double[] input) {
+        double max = Integer.MIN_VALUE;
+        for (double value : input) {
+            if (Math.abs(value) > max) {
+                max = Math.abs(value);
+            }
+        }
+        return max;
+    }
+}
