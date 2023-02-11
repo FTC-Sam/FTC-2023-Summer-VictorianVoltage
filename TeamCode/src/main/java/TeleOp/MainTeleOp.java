4da2e362782e60440c6060d4420a2bfec36355cc
samftc <chenxiang.xu56@k12.leanderisd.org>
2023-02-10 23:43:35 -0600

    Careful, there are name conflicts between imports, so make sure to import the right class.

---
+++ TeamCode/src/main/java/TeleOp/MainTeleOp.java
@@ -0,0 +1,14 @@
+package TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class MainTeleOp extends LinearOpMode {
+
+
+
+
+    @Override
+    public void runOpMode() {
+
+    }
+}
